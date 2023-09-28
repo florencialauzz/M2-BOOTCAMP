f656defcaa36726300c7c7a9e38b12850ec91627
florencialauzz <florencialauz@gmail.com>
2023-09-27 22:48:40 -0300

    ES6

--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -9,13 +9,36 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl)) {
+    resultSet.push(startEl);
+  } 
+
+  for(let i = 0; i < startEl.children.length; i++) {
+    let aux = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+    resultSet = [...resultSet, ...aux]
+  }
+
+  return resultSet;
 };
+
+
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  // 'h2'
+  // # -> id
+  // . -> class
+  // tag.class -> tag class
+  //  -> tag
+
+  if(selector[0] === '#') return 'id';
+  if(selector[0] === '.') return 'class';
+  if(selector.includes('.')) return 'tag.class';
+
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -23,16 +46,60 @@ var selectorTypeMatcher = function (selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") {
-  } else if (selectorType === "class") {
-  } else if (selectorType === "tag.class") {
-  } else if (selectorType === "tag") {
-  }
-  return matchFunction;
-};
+var matchFunctionMaker = function (selector) 
+  { // 'h2'
+    let selectorType = selectorTypeMatcher(selector); // 'tag'
+    let matchFunction;
+  
+    if (selectorType === "id") { // 'id' === 'id'
+    
+      matchFunction = function(element) { // <div id='price'></div>
+        return selector === `#${element.id}`; // '#price'
+      }
+  
+    } else if (selectorType === "class") { // 'class' === 'class'
+  
+      matchFunction = function(element) { 
+        // <img class="thumbnail lead lightback"/>
+        let classes = element.classList; // ['thumbnail', 'lead', 'lightback']
+        
+        for(let i = 0; i < classes.length; i++) {
+          if(`.${classes[i]}` === selector) return true;
+          // '.thumbnail' === '.thumbnail'
+        }
+  
+        return false;
+      }
+  
+    } else if (selectorType === "tag.class") { 
+      // 'tag.class'==='tag.class'
+  
+      matchFunction = (element) => {
+        // element -> <img class="thumbnail lead lightback"/>
+        // selector -> 'img.thumbnail'
+  
+        let [tagName, className ] = selector.split('.');
+        
+        let primerLlamado = matchFunctionMaker(tagName); // esto retorna la fn MATCHFUNCTION para evaluar el TAG
+        let llamadoDos = primerLlamado(element); // llamo a MATCHFUNCTION pasándole el elemento
+        let segundoLlamado = matchFunctionMaker(`.${className}`); //  esto retorna la fn MATCHFUNCTION para evaluar el CLASS
+        let llamadoCuatro = segundoLlamado(element); // llamo a MATCHFUNCTION pasándole el elemento
+  
+        return llamadoDos && llamadoCuatro; // 
+        // return matchFunctionMaker(tagName)(element) && matchFunctionMaker(`.${className}`)(element);
+         //               TRUE                      &&                   TRUE               => TRUE
+      }
+  
+    } else if (selectorType === "tag") { // 'tag' === 'tag'
+  
+      matchFunction = (element) => { // <h2></h2>
+        return element.tagName.toLowerCase() === selector;
+              // 'H2'.toLowerCase() -> 'h2' === 'h2'
+      }
+  
+    }
+    return matchFunction;
+  };
 
 var $ = function (selector) {
   var elements;
